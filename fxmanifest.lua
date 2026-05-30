50177a7ae3451aed92db1bf53b333ec0707e8627
kongcheng666789 <107064186+kongcheng666789@users.noreply.github.com>
2026-05-30 21:18:41 +0900

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'kongcheng'
+description 'Standalone police shield'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
